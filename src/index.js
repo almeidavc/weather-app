441201a5211fb01768086c8d8cc25494dbc5e9ee
Vicente Almeida <vicentecostaalmeida@gmail.com>
2021-09-23 16:29:54 +0100

    Add current weather summary

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 import "regenerator-runtime/runtime";
 import { getWeatherData } from "./weatherApi";
 
-getWeatherData("london").then(console.log);
+const sumLocation = document.querySelector(".sum__location");
+const sumDescription = document.querySelector(".sum__description");
+const sumTempNow = document.querySelector(".sum__temp-now");
+const sumTempMax = document.querySelector(".sum__temp-max");
+const sumTempMin = document.querySelector(".sum__temp-min");
+
+async function loadWeatherInfo(location) {
+  displayWeatherInfo(await getWeatherData(location));
+}
+
+function displayWeatherInfo(weatherData) {
+  sumLocation.textContent = weatherData.location;
+  sumDescription.textContent = weatherData.description;
+  setTemp(sumTempNow, weatherData.tempNow);
+  setTemp(sumTempMax, weatherData.tempMax, "H:");
+  setTemp(sumTempMin, weatherData.tempMin, "L:");
+}
+
+function setTemp(element, temp, prefix) {
+  element.textContent = `${prefix ? prefix : ""}${temp}°`;
+}
+
+loadWeatherInfo("london");
--- src/weatherApi.js
+++ src/weatherApi.js
@@ -2,11 +2,24 @@ const API_KEY = "7f25452f6b74ed11e1e1effa780583dc";
 
 async function getWeatherData(locationName) {
   try {
-    const { lat, lon } = (await fetchCoords(locationName))[0];
-    return fetchWeatherData(lat, lon);
+    const { name: location, lat, lon } = (await fetchCoords(locationName))[0];
+    return filterWeatherData(
+      location,
+      await fetchWeatherData(lat, lon, "metric")
+    );
   } catch (error) {
     console.log(error);
   }
+}
+
+function filterWeatherData(location, weatherData) {
+  return {
+    location,
+    tempNow: weatherData.current.temp,
+    tempMax: weatherData.daily[0].temp.max,
+    tempMin: weatherData.daily[0].temp.min,
+    description: weatherData.current.weather[0].description,
+  };
 }
 
 async function fetchCoords(locationName) {
@@ -18,11 +31,11 @@ async function fetchCoords(locationName) {
   );
 }
 
-async function fetchWeatherData(lat, lon) {
+async function fetchWeatherData(lat, lon, units) {
   return fetchAndParse(
     getApiCall(
       "http://api.openweathermap.org/data/2.5/onecall",
-      `lat=${lat}&lon=${lon}`
+      `lat=${lat}&lon=${lon}&units=${units}`
     )
   );
 }
