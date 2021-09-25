026b61390f0634a805edd132258aed1a612e51bf
Vicente Almeida <vicentecostaalmeida@gmail.com>
2021-09-25 14:53:06 +0100

    Add bar below forecast buttons and other style changes

--- src/Extra.js
+++ src/Extra.js
@@ -4,6 +4,7 @@ const hourlyButton = document.querySelector(".hourly__button");
 const dailyButton = document.querySelector(".daily__button");
 const hourlyContainer = document.querySelector(".hourly");
 const dailyContainer = document.querySelector(".daily");
+const buttonsBar = document.querySelector(".buttons__bottom-bar");
 
 export default class Extra {
   static hourlyForecast;
@@ -38,10 +39,22 @@ export default class Extra {
   static displayHourlyForecast() {
     this.dailyForecast.hide();
     this.hourlyForecast.display();
+    this.highlightHourlyButton();
   }
 
   static displayDailyForecast() {
     this.hourlyForecast.hide();
     this.dailyForecast.display();
+    this.hightlightDailyButton();
+  }
+
+  static highlightHourlyButton() {
+    buttonsBar.classList.remove("hl-daily");
+    buttonsBar.classList.add("hl-hourly");
+  }
+
+  static hightlightDailyButton() {
+    buttonsBar.classList.remove("hl-hourly");
+    buttonsBar.classList.add("hl-daily");
   }
 }
